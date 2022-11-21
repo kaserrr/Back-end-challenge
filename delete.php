9d4142c831350108edf2b27a47c78297e6eb64e6
kaserrr <99057099@mydavinci.nl>
2022-11-21 13:05:29 +0100

    delete function
    The delete function is working.

---
+++ delete.php
@@ -0,0 +1,10 @@
+<?php
+    include_once 'functions.php';
+
+    if(isset($_GET['id'])){
+        $id = $_GET['id'];
+        deleteCategory($id);
+        redirectPage("./index.php");
+        echo $id;
+    }
+?>
--- functions.php
+++ functions.php
@@ -1,6 +1,21 @@
 <?php
+    include_once 'connection.php';
+
+    function redirectPage($location) {
+        header('location: ' . $location);
+        exit;
+    }
+
     function getCategories() {
         $query = "SELECT * FROM categories";
+        $item = $GLOBALS['db']->prepare($query);
+        $item->execute();
+
+        return $item->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    function deleteCategory($id) {
+        $query = "DELETE FROM categories WHERE id = $id";
         $item = $GLOBALS['db']->prepare($query);
         $item->execute();
 
